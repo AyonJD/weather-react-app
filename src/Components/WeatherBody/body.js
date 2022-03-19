d5d7ccb37de8449b9b014546aaee6f3f1fb93bb1
Ayon <ayonjodder177@gmail.com>
2022-03-19 16:37:02 +0800

    Weather body component added

--- src/Components/WeatherBody/body.js
+++ src/Components/WeatherBody/body.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const body = () => {
+const Body = () => {
     return (
         <div>
-            
+            <div className="row">
+                    <div className="col-md-3 register-left">
+                        <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
+                        <h3>Welcome</h3>
+                        <p>You are 30 seconds away from earning your own money!</p>
+                        <input type="submit" name="" value="Login"/><br/>
+                    </div>
+                    <div classname="col-md-9 register-right">
+                        <div classname="ocean">
+                            <div classname="wave"></div>
+                            <div classname="wave"></div>
+                        </div>
+                    </div>
+                </div>
         </div>
     );
 };
 
-export default body;
+export default Body;
